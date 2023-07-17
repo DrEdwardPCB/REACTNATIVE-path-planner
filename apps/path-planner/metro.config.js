e0f4884bb27fc02c868f125edff174c9f4f4f6a9
DrEdwardPCB <eternal.edward1997@gmail.com>
2023-07-18 00:19:56 +0800

    add expo source

---
+++ apps/path-planner/metro.config.js
@@ -0,0 +1,40 @@
+const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
+
+// Find the project and workspace directories
+const projectRoot = __dirname;
+// This can be replaced with `find-yarn-workspace-root`
+const workspaceRoot = path.resolve(projectRoot, "../..");
+
+const config = getDefaultConfig(projectRoot);
+const { makeMetroConfig } = require("@rnx-kit/metro-config");
+const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks");
+const symlinkResolver = MetroSymlinksResolver();
+
+const requestResolver = (context, moduleName, platform, realName) => {
+	// patch for unpatched dependencies
+	// e.g. react-native-version-check-expo
+	if (moduleName === "@unimodules/core") {
+		const expoModules = "expo-modules-core";
+		return symlinkResolver(context, expoModules, platform, expoModules);
+	}
+
+	return symlinkResolver(context, moduleName, platform, realName);
+};
+
+// // 1. Watch all files within the monorepo
+// config.watchFolders = [workspaceRoot];
+// // 2. Let Metro know where to resolve packages and in what order
+// config.resolver.nodeModulesPaths = [path.resolve(projectRoot, "node_modules"), path.resolve(workspaceRoot, "node_modules")];
+// // 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
+// config.resolver.disableHierarchicalLookup = true;
+
+module.exports = {
+	...config,
+	...makeMetroConfig({
+		projectRoot: __dirname,
+		resolver: {
+			resolveRequest: requestResolver,
+		},
+	}),
+};
